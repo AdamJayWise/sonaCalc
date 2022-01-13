ab4902e22a88f5e26d804b7b73ae13c01c5459d4
AdamWise <wise.adam.jay@gmail.com>
2022-01-13 09:54:47 -0500

    added sona 2.0

--- js/cameraInfo.js
+++ js/cameraInfo.js
@@ -1,22 +1,7 @@
 var cameraInfo = {
 
-    sona42b11 : {
-        displayName : "Sona 4.2B-11 - USB 3.1",
-        columns : 2048,
-        rows : 2048,
-        bitModes : [12, 16],
-        rowTimeUs : {12 : 20.52/2, 16 : 20.52},
-        frameTimeUs : {12 : 21.012 * 1000, 16 : 42.025 * 1000},
-        hasGlobalShutter : false,
-        globalClear : false,
-        interface : "usb3",
-        bitDepth : 12,
-        numReadouts : 1,
-        timings : 'sona11',
-    },
-
     sona42b6 : {
-        displayName : "Sona 4.2B-6 - USB 3.1",
+        displayName : "Sona 4.2B-6U",
         columns : 2048,
         rows : 2048,
         bitModes : [12, 16],
@@ -31,7 +16,7 @@ var cameraInfo = {
     },
 
     sona42b6x : {
-        displayName : "Sona 4.2B-6 - CoaXPress",
+        displayName : "Sona 4.2B-6X",
         columns : 2048,
         rows : 2048,
         bitModes : [12, 16],
@@ -43,6 +28,36 @@ var cameraInfo = {
         bitDepth : 12,
         numReadouts : 1,
         timings : "sona6",
+    },
+
+    sona42b11 : {
+        displayName : "Sona 4.2B-11",
+        columns : 2048,
+        rows : 2048,
+        bitModes : [12, 16],
+        rowTimeUs : {12 : 20.52/2, 16 : 20.52},
+        frameTimeUs : {12 : 21.012 * 1000, 16 : 42.025 * 1000},
+        hasGlobalShutter : false,
+        globalClear : false,
+        interface : "usb3",
+        bitDepth : 12,
+        numReadouts : 1,
+        timings : 'sona11',
+    },
+
+    sona2b11 : {
+        displayName : "Sona 2.0B-11",
+        columns : 1400,
+        rows : 1400,
+        bitModes : [12, 16],
+        rowTimeUs : {12 : 20.52/2, 16 : 20.52},
+        frameTimeUs : {12 : 21.012 * 1000 * (1400/2048), 16 : 42.025 * 1000 * (1400/2048)},
+        hasGlobalShutter : false,
+        globalClear : false,
+        interface : "usb3",
+        bitDepth : 12,
+        numReadouts : 1,
+        timings : 'sona11',
     },
 
 }
